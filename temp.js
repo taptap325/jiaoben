9a4347f1a1b18cb02a2baab8196b703b980c3200
geensu <1278224185@qq.com>
2022-09-03 21:33:54 +0800

    add license

--- temp.js
+++ temp.js
@@ -6,6 +6,7 @@
 // @author       geensu
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @license      AGPL License
 // @grant        none
 // ==/UserScript==
 
