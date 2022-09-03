af03f59c68af373783f6cb29c548da27e30d414b
geensu <1278224185@qq.com>
2022-09-03 21:29:11 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new script
+// @author       geensu
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('hello world')
+
+    // Your code here...
+})();
