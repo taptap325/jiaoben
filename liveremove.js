299b8fb0d0255592577a1ba9a45f60808b27f128
geensu <1278224185@qq.com>
2022-09-04 10:43:47 +0800

    zh_live

--- liveremove.js
+++ liveremove.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   去除b站播放器 
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  去除b站多余播放器
+// @description:zh-CN  去除b站多余播放器
 // @author       geensu
 // @match        https://live.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
