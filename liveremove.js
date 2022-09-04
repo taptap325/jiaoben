874528e36fb0764052d0921b063f96aa3b079137
geensu <1278224185@qq.com>
2022-09-04 10:12:10 +0800

    first_bli

---
+++ liveremove.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         liveremove
+// @name:zh-CN   去除b站播放器 
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  去除b站多余播放器
+// @author       geensu
+// @match        https://live.bilibili.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @license      AGPL License
+// @grant        none
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+
+})();
